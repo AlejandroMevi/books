19abac260d7b60b8893fa16fc1221dc65e5543a8
AlejandroMevi <106292726+AlejandroMevi@users.noreply.github.com>
2023-08-30 12:48:44 -0600

    BottomNavigate
    BottomNavigate

--- app/src/main/java/com/example/books/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/books/ui/home/MainActivity.kt
@@ -5,34 +5,57 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import com.example.books.R
 import com.example.books.core.ApiResponceStatus
 import com.example.books.databinding.ActivityMainBinding
 import com.example.books.ui.home.vm.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val bookMainViewModel : MainViewModel by viewModels()
+    private val bookMainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView()
         buttons()
     }
 
-    private fun buttons() {
-        binding.callService.setOnClickListener { callService() }
+    private fun initView() {
+        loadFragment(BooksFragment(), "Books")
     }
 
-    private fun callService(){
+    private fun buttons() {
+        //binding.callService.setOnClickListener { callService() }
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(BooksFragment(), "Books")
+                    true
+                }
+
+                R.id.fav -> {
+                    loadFragment(FavFragment(), "Fav")
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private fun callService() {
         bookMainViewModel.getBooks("a")
         statusObserve()
-        bookMainViewModel.data.observe(this){response ->
+        bookMainViewModel.data.observe(this) { response ->
             if (response != null) {
                 println(response)
                 println(response.items.size)
             }
         }
     }
+
     private fun statusObserve() {
         bookMainViewModel.status.observe(this) { status ->
             if (status != null) {
@@ -40,9 +63,11 @@ class MainActivity : AppCompatActivity() {
                     is ApiResponceStatus.Loading -> {
                         Toast.makeText(this, "Cargando", Toast.LENGTH_SHORT).show()
                     }
+
                     is ApiResponceStatus.Success -> {
                         clearService()
                     }
+
                     is ApiResponceStatus.Error -> {
                         clearService()
                         val errorCode = status.messageId
@@ -52,8 +77,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     private fun clearService() {
         bookMainViewModel.status.removeObservers(this)
         bookMainViewModel.status.value = null
+    }
+
+    private fun loadFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment, tag)
+            .commit()
     }
 }
